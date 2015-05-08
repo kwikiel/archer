1115437f1933bfb112267ce4bb56d1c80a13ff3f
Kacper <random8902@gmail.com>
2015-05-08 14:44:21 +0200

    Removing debugging print()

--- investments.py
+++ investments.py
@@ -24,7 +24,7 @@ def weird(id):
         totals.append(
                 [float(inv[i]['amount']), float(inv[i]['rate'])]
                     )
-        print("Amount: {0}, rate: {1}".format(inv[i]['amount'], inv[i]['rate']))
+    #Debug    print("Amount: {0}, rate: {1}".format(inv[i]['amount'], inv[i]['rate']))
 
 
     #Sorting by rate value
@@ -35,25 +35,19 @@ def weird(id):
     raw = requests.get("https://api.bitlendingclub.com/api/loan/{id}".format(id=id))
     done = True
     #Caveat: Not 100% -> max rate could be same as current biggest
+    max_rate = 10
     for idx,part in enumerate(sbr):
         if total>float(raw.json()['loans'][0]['amount']) and done:
             max_rate = rates[-1]
-            print("Maximum rate {}".format(rates[-1]))
+            #print("Maximum rate {}".format(rates[-1]))
             done=False
         total = total + part[0]
         values.append(total)
         rates.append(part[1])
-
-    plt.plot(values,rates)
-    plt.ylabel('Nominal  %') #Ylabel
-    plt.xlabel('Bitcoin amount')
-    #Getting metadata about loan
-    cutoff = float(raw.json()['loans'][0]['amount'])
-    print("Loan amount: {}".format(cutoff))
-    plt.axvline(cutoff, color='r', linewidth=10)
-    #Different id
-    filename = "/home/kacper/lendon/static/images/{0}.png".format(id)
-    plt.savefig(filename)
+    #Nominal rate
+    term = raw.json()['loans'][0]['term']
+    real_rate = float(max_rate*(365.0/term))
+    return id,real_rate
 
 def loan_chart(id):
     target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
@@ -83,7 +77,7 @@ def loan_chart(id):
         values.append(total)
         rates.append(part[1])
 
-    plt.plot(values,rates, 'ro')
+    plt.plot(values,rates)
     plt.ylabel('Nominal  %') #Ylabel
     plt.xlabel('Bitcoin amount')
     #Getting metadata about loan

fdb9fb180d11c57135977a6da7402acf141c2e3c
Kacper <random8902@gmail.com>
2015-05-08 15:33:33 +0200

    Feature: list of funded loans

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import render_template
-from investments import loan_chart
+from investments import loan_chart, get_funded_loans
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/list_loans')
+def listing():
+   listing = get_funded_loans()
+   return render_template("list_loans.html", listing=listing)
+
 
 @app.route('/loan/<int:id>')
 def charts(id):

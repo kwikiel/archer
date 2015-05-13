2273389bd1cfafa9958394ba2f22dc4d33a43db9
Kacper <random8902@gmail.com>
2015-05-13 22:10:43 +0200

    Feature: best rate for not funded loans

--- investments.py
+++ investments.py
@@ -7,12 +7,27 @@ def get_funded_loans():
     """
     Fetching current 100% loans available to bid
     """
-    raws = requests.get("http://api.bitlendingclub.com/api/loans?status=funding&denomination=btc&fundedFrom=90")
+    raws = requests.get("http://api.bitlendingclub.com/api/loans?status=funding&denomination=btc&fundedFrom=5")
     loan_list = []
     for loan in raws.json()['loans']:
         loan_list.append(loan['id'])
 
     return loan_list
+
+
+def best_rate(id):
+    """
+    Best nominal rate for investments
+    """
+    target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
+    r = requests.get(target)
+    totals = []
+    inv = r.json()['investments']
+    for i in range(0, len(r.json()['investments'])):
+        totals.append([float(inv[i]['amount']), float(inv[i]['rate'])])
+    #Sorting by rate value
+    sbr = sorted(totals, key=itemgetter(1))
+    return sbr[-1]
 
 def weird(id):
     target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
@@ -24,31 +39,11 @@ def weird(id):
         totals.append(
                 [float(inv[i]['amount']), float(inv[i]['rate'])]
                     )
-    #Debug    print("Amount: {0}, rate: {1}".format(inv[i]['amount'], inv[i]['rate']))
 
 
     #Sorting by rate value
     sbr = sorted(totals, key=itemgetter(1))
-    total = 0 # total loan amount
-    max_rate = 0 
-    values = []
-    rates = []
-    raw = requests.get("https://api.bitlendingclub.com/api/loan/{id}".format(id=id))
-    done = True
-    #Caveat: Not 100% -> max rate could be same as current biggest
-    for idx,part in enumerate(sbr):
-        if total>float(raw.json()['loans'][0]['amount']) and done:
-            max_rate = rates[-1]
-            #print("Maximum rate {}".format(rates[-1]))
-            done=False
-        total = total + part[0]
-        values.append(part[0])
-        rates.append(part[1])
-    #Nominal rate
-    term = raw.json()['loans'][0]['term']
-    real_rate = float(max_rate*(365.0/term))
-    caps = [(x, y) for x in values for y in rates]
-    return sorted(caps, key=itemgetter(1)), max_rate
+    return sbr[-1]
 
 def loan_chart(id):
     target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
@@ -65,6 +60,7 @@ def loan_chart(id):
 
     #Sorting by rate value
     sbr = sorted(totals, key=itemgetter(1))
+
     total = 0 # total loan amount
     values = []
     rates = []

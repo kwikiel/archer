1732037b78e7f023394cf38b4395949f418f0fef
Kacper <random8902@gmail.com>
2015-05-16 02:41:42 +0200

    Feature: Dict based approach

---
+++ agregate.py
@@ -0,0 +1,98 @@
+import requests
+from operator import itemgetter
+from matplotlib import pyplot as plt
+
+
+def get_funded_loans():
+    """
+    Fetching current 100% loans available to bid
+    """
+    raws = requests.get("http://api.bitlendingclub.com/api/loans?status=funding&denomination=btc&fundedFrom=5")
+    loan_list = []
+    for loan in raws.json()['loans']:
+        loan_list.append(loan['id'])
+
+    return loan_list
+
+def summary(id):
+    #Part to separate to make independent tests
+    target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
+    r = requests.get(target)
+    totals = []
+    inv = r.json()['investments']
+    for i in range(0, len(r.json()['investments'])):
+        totals.append([float(inv[i]['amount']), float(inv[i]['rate'])])
+    #Sorting by rate value
+    sbr = sorted(totals, key=itemgetter(1))
+    total = 0 # total loan amount
+    max_rate = 0 #Should be non zero if funded
+    values = []
+    rates = []
+    raw = requests.get("https://api.bitlendingclub.com/api/loan/{id}".format(id=id))
+    done = True
+    #Caveat: Not 100% -> max rate could be same as current biggest
+    for idx,part in enumerate(sbr):
+        if total>float(raw.json()['loans'][0]['amount']) and done:
+            max_rate = rates[-1]
+            #print("Maximum rate {}".format(rates[-1]))
+            done=False
+        total = total + part[0]
+        values.append(part[0])
+        rates.append(part[1])
+        last_inv = part[1]
+    #Nominal rate and real rate etc
+
+    info = {}
+    term = raw.json()['loans'][0]['term']
+    info['term'] = term
+    if max_rate:
+        real_rate = float(max_rate*(365.0/term))
+    else:
+        real_rate = float(last_inv*(365.0/term))
+    info['real_rate'] = real_rate
+    caps = [(x, y) for x in values for y in rates]
+    investments =  sorted(caps, key=itemgetter(1))
+    info['investments'] = investments
+    info['max_rate'] = max_rate
+    info['last'] = last_inv
+    info['id'] = id
+    return info
+
+def loan_chart(id):
+    target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
+    r = requests.get(target)
+
+    totals = []
+    inv = r.json()['investments']
+    for i in range(0, len(r.json()['investments'])):
+        totals.append(
+                [float(inv[i]['amount']), float(inv[i]['rate'])]
+                    )
+        print("Amount: {0}, rate: {1}".format(inv[i]['amount'], inv[i]['rate']))
+
+
+    #Sorting by rate value
+    sbr = sorted(totals, key=itemgetter(1))
+    total = 0 # total loan amount
+    values = []
+    rates = []
+    raw = requests.get("https://api.bitlendingclub.com/api/loan/{id}".format(id=id))
+    done = True
+    for idx,part in enumerate(sbr):
+        if total>float(raw.json()['loans'][0]['amount']) and done:
+            print("Maximum rate {}".format(rates[-1]))
+            done=False
+        total = total + part[0]
+        values.append(total)
+        rates.append(part[1])
+
+    plt.plot(values,rates)
+    plt.ylabel('Nominal  %') #Ylabel
+    plt.xlabel('Bitcoin amount')
+    #Getting metadata about loan
+    cutoff = float(raw.json()['loans'][0]['amount'])
+    print("Loan amount: {}".format(cutoff))
+    plt.axvline(cutoff, color='r', linewidth=10)
+    #Different id
+    filename = "static/images/{0}.png".format(id)
+    plt.savefig(filename)

eac72720a5d64ac4e0cb22adb5e91539ffba3960
Kacper <random8902@gmail.com>
2015-05-16 15:11:18 +0200

    Problem: locales

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import render_template, url_for
 from investments import loan_chart, get_funded_loans, weird
+from agregate import *
 from operator import itemgetter
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
-
+#Meh
 @app.route('/shark')
 def shark():
    loans = get_funded_loans()
@@ -31,8 +32,8 @@ def listing():
 @app.route('/loan/<int:id>')
 def charts(id):
    loan_chart(id)
-   investments = weird(id)
-   return render_template("loan.html", id=id, investments = investments)
+   all_data = summary(id)
+   return render_template("loan.html", id=id, all_data = all_data)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000, debug=True)

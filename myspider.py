6ec87fe4dfefe1616ea797f0142bdddf28a8c70d
Kacper <random8902@gmail.com>
2015-05-23 16:42:30 +0200

    Various things

--- myspider.py
+++ myspider.py
@@ -5,13 +5,14 @@ from threading import Thread
 
 
 URL = 'https://bitlendingclub.com/user/index/id/'
+SUFFIX = '/statuses/1/amount/0.00000000%3B0.00000000/invested/true/notInvested/true/reputation/-2000%3B2000/timeLeft/0%3B0/perPage//denomination/1%2C2%2C3/sort-by/apr/direction/desc'
 XPATH = '//*[@id="main-wrapper"]/div[1]/div/div[4]/ul/li[1]/a'
-BEGIN = 1 
-END = 30
+BEGIN = 13880
+END = 13900
 
 
 def face(id):
-    page = requests.get(URL + str(id))
+    page = requests.get(URL + str(id)+ SUFFIX)
     tree = html.fromstring(page.text)
     face_id = tree.xpath(XPATH)
     return face_id[0].get('href') if face_id else 'Empty'

80ef03a7bd1bda1f04a071e5a85c4e45b32fb1f7
Kacper Wikiel <random8902@gmail.com>
2015-11-20 23:31:08 +0100

    Brutal refactor

--- agregate.py
+++ agregate.py
@@ -1,6 +1,5 @@
 import requests
 from operator import itemgetter
-from matplotlib import pyplot as plt
 
 
 def get_funded_loans():
@@ -61,43 +60,3 @@ def summary(id):
     info['real_rate'] = real_rate
     info['max_rate'] = max_rate
     return info
-
-
-def loan_chart(id):
-    target = "https://api.bitlendingclub.com/api/investments/{id}".format(id=id)
-    r = requests.get(target)
-
-    totals = []
-    inv = r.json()['investments']
-    for i in range(0, len(r.json()['investments'])):
-        totals.append(
-                [float(inv[i]['amount']), float(inv[i]['rate'])]
-                    )
-        print("Amount: {0}, rate: {1}".format(inv[i]['amount'], inv[i]['rate']))
-
-
-    #Sorting by rate value
-    sbr = sorted(totals, key=itemgetter(1))
-    total = 0 # total loan amount
-    values = []
-    rates = []
-    raw = requests.get("https://api.bitlendingclub.com/api/loan/{id}".format(id=id))
-    done = True
-    for idx,part in enumerate(sbr):
-        if total>float(raw.json()['loans'][0]['amount']) and done:
-            print("Maximum rate {}".format(rates[-1]))
-            done=False
-        total = total + part[0]
-        values.append(total)
-        rates.append(part[1])
-
-    plt.plot(values,rates)
-    plt.ylabel('Nominal  %') #Ylabel
-    plt.xlabel('Bitcoin amount')
-    #Getting metadata about loan
-    cutoff = float(raw.json()['loans'][0]['amount'])
-    print("Loan amount: {}".format(cutoff))
-    plt.axvline(cutoff, color='r', linewidth=10)
-    #Different id
-    filename = "static/images/{0}.png".format(id)
-    plt.savefig(filename)

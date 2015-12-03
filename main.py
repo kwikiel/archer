0fdf9227eb7ec251e9d8fcdffc7038d130f318f1
Kacper Wikiel <random8902@gmail.com>
2015-12-03 17:01:32 +0100

    PEP8

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python
 
 from flask import Flask
-from flask import render_template, url_for
-from investments import loan_chart, get_funded_loans, weird
-from agregate import *
+from flask import render_template
+from investments import get_funded_loans, summary
 from operator import itemgetter
-import sys
 from flask.ext.cache import Cache
 
 app = Flask(__name__)
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+
 
 @app.route('/')
 def index():
     return render_template("cards.html")
 
+
 @app.route('/list_loans')
 def listing():
-   brt = cache.get('loans')
-   if brt is None:
-      listing = get_funded_loans()
-      superlist = []
-      # refactor this below
-      for loan in listing:
-        superlist.append( [ [loan], [ summary(loan)['real_rate'] ]] )
-      supersorted = sorted(superlist, key=itemgetter(1))
-      brt = supersorted[::-1]
-      cache.set('loans', brt, timeout=6*60)
-   return render_template("list_loans.html", superlist=brt)
+    brt = cache.get('loans')
+    if brt is None:
+        listing = get_funded_loans()
+        superlist = []
+        # refactor this below
+        for loan in listing:
+            superlist.append([[loan], [summary(loan)['real_rate']]])
+            supersorted = sorted(superlist, key=itemgetter(1))
+            brt = supersorted[::-1]
+        cache.set('loans', brt, timeout=6*60)
+    return render_template("list_loans.html", superlist=brt)
 
 
 if __name__ == '__main__':
-   app.config['DEBUG'] = False
-   app.run(host='0.0.0.0', port=5000)
+    app.config['DEBUG'] = False
+    app.run(host='0.0.0.0', port=5000)
